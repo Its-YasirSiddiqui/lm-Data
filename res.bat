c299c797ce581ea8e707ff3eed82ac2726b32863
Yasir Siddiqui <its.yasirsiddiqui@gmail.com>
2024-10-09 00:11:20 +0500

    Update res.bat

--- res.bat
+++ res.bat
@@ -1,18 +1,18 @@
 @echo off
 setlocal
 
-REM Define the download URL and destination path
-set url=httpsgithub.comIts-YasirSiddiquilm-Datarawrefsheadsmainnircmd.exe
-set dest=Cnircmd.exe
+REM Define the download URL for nircmd.exe and destination path
+set "url=https://github.com/Its-YasirSiddiqui/lm-Data/raw/refs/heads/main/nircmd.exe"
+set "dest=C:\nircmd.exe"
 
-REM Download nircmd.exe using PowerShell
-PowerShell -Command Invoke-WebRequest -Uri '%url%' -OutFile '%dest%'
+REM Download nircmd.exe using curl
+curl -s -L -o "%dest%" "%url%"
 
 REM Check if the download was successful
-if exist %dest% (
+if exist "%dest%" (
     echo NirCmd downloaded successfully.
     REM Change the screen resolution
-    %dest% setdisplay 1680 1050 32
+    "%dest%" setdisplay 1680 1050 32
 ) else (
     echo Failed to download NirCmd.
 )
