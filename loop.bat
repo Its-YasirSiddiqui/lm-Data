94dffcdb27420e8e92975eb1578f8b0a3e63379a
Yasir Siddiqui <its.yasirsiddiqui@gmail.com>
2024-10-08 22:51:49 +0500

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/Its-YasirSiddiqui/lm-Data/refs/heads/main/loop.py?token=GHSAT0AAAAAACYUUV6YWGIAPYMQTTCCMFFIZYFN6EA
+curl -s -L -o loop.py https://raw.githubusercontent.com/Its-YasirSiddiqui/lm-Data/refs/heads/main/loop.py
 python loop.py
